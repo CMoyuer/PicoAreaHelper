fab64a1d705057e68b29175c11ed05b493384357
Moyuer <1531964875@qq.com>
2022-10-15 15:44:57 +0800

    修复bat编码问题

--- 1.安装USB驱动.bat
+++ 1.安装USB驱动.bat
@@ -1,7 +1,7 @@
 @echo off
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cls
-pnputil /add-driver %~dp0\usb_driver\android_winusb.inf
+pnputil /add-driver %~dp0\usb_driver\*.inf
 echo ===================================================
 echo Ö´ÐÐÍê³É
 pause
